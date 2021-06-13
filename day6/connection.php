e79bad6780776652bd30ad88b0478b69624f2725
diksha sinh <84770423+diksha1212sinh@users.noreply.github.com>
2021-06-13 22:21:37 +0530

    Update connection.php

--- day6/connection.php
+++ day6/connection.php
@@ -7,7 +7,7 @@ $dbname = "db_internship";
 $connection= mysqli_connect($host, $username, $passwd, $dbname);
 
 $q=mysqli_query($connection, 
-        "insert into tbl_user(user_name,user_gender,user_mobile) values('meet','male','123')") or die ("error". mysqli_error($connection));
+        "insert into tbl_user(user_name,user_gender,user_mobile) values('diksha','female','123')") or die ("error". mysqli_error($connection));
 
 if($q)
 {
